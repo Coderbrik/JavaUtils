5e8e9e56ce6e4d467667a34318244b6f708fceb4
psrcek <p.srcek@gmail.com>
2016-09-25 00:37:07 +0200

    Moved SkullClick out of folders

---
+++ SkullClick.java
@@ -0,0 +1,48 @@
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Skull;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import net.md_5.bungee.api.ChatColor;
+import redempt.moduleloader.Module;
+import redempt.moduleloader.ModuleLoader;
+
+public class SkullClick extends Module implements Listener {
+	
+	@Override
+	public void onEnable() {
+		ModuleLoader.registerEvent(this);
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public String getName() {
+		return "SkullClick";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Shows a skull's owner when it is right clicked";
+	}
+	
+	@EventHandler
+	public void onClick(PlayerInteractEvent e) {
+		if (e.getAction() == Action.RIGHT_CLICK_BLOCK && !e.isCancelled()) {
+			BlockState block = e.getClickedBlock().getState();
+			if (block instanceof Skull) {
+				Skull skull = (Skull) block;
+				String owner = skull.getOwner();
+				if (owner == null || owner.equals("")) {
+					e.getPlayer().sendMessage(ChatColor.YELLOW + "That skull has no owner.");
+				} else {
+					e.getPlayer().sendMessage(ChatColor.YELLOW + "That's " + owner + ".");
+				}
+			}
+		}
+	}
+	
+}
