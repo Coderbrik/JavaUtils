c96149fd2b379fa2fbe91700e60f7553e9491386
psrcek <p.srcek@gmail.com>
2016-10-15 22:05:08 +0200

    Added Login timer

--- src/com/redstoner/javautils/loginSecurity/LoginSecurity.java
+++ src/com/redstoner/javautils/loginSecurity/LoginSecurity.java
@@ -1,11 +1,22 @@
 package com.redstoner.javautils.loginSecurity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.nemez.cmdmgr.Command;
 import com.redstoner.moduleLoader.Module;
 
-public class LoginSecurity extends Module {
+public class LoginSecurity extends Module implements Listener {
+	private List<Player> loggingIn;
+	
 	@Override
 	public String getName() {
 		return "LoginSecurity";
@@ -14,6 +25,11 @@ public class LoginSecurity extends Module {
 	@Override
 	public String getDescription() {
 		return "Allows you to protect your account with a password";
+	}
+	
+	@Override
+	public void onEnable() {
+		loggingIn = new ArrayList<>();
 	}
 	
 	@Command(hook = "register")
@@ -33,6 +49,43 @@ public class LoginSecurity extends Module {
 	
 	@Command(hook = "rmpass")
 	public void rmpass(CommandSender sender) {
+		
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		
+		loggingIn.add(player);
+		
+		Thread playerLoginThread = new Thread() {
+			@Override
+			public void run() {
+				Long endTime = System.currentTimeMillis() + 60000;
+				
+				while (endTime > System.currentTimeMillis()) {
+					if (!player.isOnline()) {
+						loggingIn.remove(player);
+					}
+					
+					//this is seperate to check for logins
+					if (!loggingIn.contains(player)) {
+						player.sendMessage(ChatColor.GREEN + "Successfully logged in!");
+						break;
+					}
+				}
+				
+				if (loggingIn.contains(player)) {
+					player.kickPlayer("You didn't login in time!");
+				}
+			}
+		};
+		
+		playerLoginThread.start();
+	}
+	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent e) {
 		
 	}
 	
