a434fe92813c46875e6d23efc8ee819704cad2d3
ViperLordX <destructinator.robert47@gmail.com>
2016-10-16 12:43:44 -0400

    Fixed directories again

--- src/com/redstoner/javautils/adminnotes/AdminNotes.java
+++ src/com/redstoner/javautils/adminnotes/AdminNotes.java
@@ -24,7 +24,7 @@ import net.md_5.bungee.api.ChatColor;
 
 public class AdminNotes extends Module implements Listener {
 	
-	File file = new File("plugins/ModuleLoader/config/adminnotes.json");
+	File file = new File(ModuleLoader.getLoader().getDataFolder(), "adminnotes.json");
 	
 	@Override
 	public String getDescription() {
