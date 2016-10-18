2d2dad8adba1a39938d1537166315d21e30f20e6
psrcek <p.srcek@gmail.com>
2016-10-18 20:58:01 +0200

    Cleaned up some things I missed
    Dat fail tho

--- src/com/redstoner/javautils/warn/Warn.java
+++ src/com/redstoner/javautils/warn/Warn.java
@@ -4,22 +4,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 
 import com.nemez.cmdmgr.Command;
 import com.redstoner.moduleLoader.Module;
 
-public class Warn extends Module implements Listener {
+public class Warn extends Module {
 	//@formatter:off
 	@Override
 	public String getCmdManagerString() {
 		return "command warn {"
-				+ "perm utils.warn;"
-				
 				+ "[string:name] {"
 					+ "run warn name;"
 					+ "type player;"
 					+ "help Warns other players about definite lag;"
+					+ "perm utils.warn;"
 				+ "}"
 			+ "}"
 			
@@ -28,6 +26,7 @@ public class Warn extends Module implements Listener {
 					+ "run warnp name;"
 					+ "type player;"
 					+ "help Warns other players about possible lag;"
+					+ "perm utils.warn;"
 				+ "}"
 			+ "}";
 	}
@@ -36,13 +35,13 @@ public class Warn extends Module implements Listener {
 	@Command(hook = "warn")
 	public void warn_normal(CommandSender sender, String name) {
 		Player s = (Player) sender;
-		Bukkit.broadcastMessage(ChatColor.AQUA + " = " + ChatColor.DARK_GREEN + ChatColor.BOLD + "Lag incomming! " + ChatColor.WHITE + "-" + s.getDisplayName());
+		Bukkit.broadcastMessage(ChatColor.AQUA + " = " + ChatColor.DARK_GREEN + ChatColor.BOLD + "Lag incomming! " + ChatColor.RESET + "-" + s.getDisplayName());
 	}
 	
 	@Command(hook = "warnp")
 	public void warn_possible(CommandSender sender, String name) {
 		Player s = (Player) sender;
-		Bukkit.broadcastMessage(ChatColor.AQUA + " = " + ChatColor.DARK_GREEN + ChatColor.BOLD + "Possible lag incomming! " + ChatColor.WHITE + "-" + s.getDisplayName());
+		Bukkit.broadcastMessage(ChatColor.AQUA + " = " + ChatColor.DARK_GREEN + ChatColor.BOLD + "Possible lag incomming! " + ChatColor.RESET + "-" + s.getDisplayName());
 	}
 	
 	@Override
