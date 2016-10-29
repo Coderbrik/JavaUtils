722bb633ef0d5fc6a7a0e70ab8180658c4310310
Will Garrett <willgarrett610@gmail.com>
2016-10-29 13:55:25 -0600

    Changed format to json

--- src/com/redstoner/javautils/damnspam/SpamInput.java
+++ src/com/redstoner/javautils/damnspam/SpamInput.java
@@ -20,13 +20,37 @@ public class SpamInput {
 	}
 	
 	protected static SpamInput fromString(String string){
-		String[] args = string.split(";");
-		return new SpamInput(Bukkit.getPlayer(UUID.fromString(args[0])),Double.parseDouble(args[1]),Double.parseDouble(args[2]),Double.parseDouble(args[3]));
+		string = string.substring(1,string.length() - 1);
+		String[] args = string.split(",");
+		Player player = null;
+		double timeoutOff = 0;
+		double timeoutOn = 0;
+		double lastTime = 0;
+		for(String s : args){
+			String[] vals = s.split(":");
+			String name = vals[0].replace(" ", "").replace("\"", "");
+			String val = vals[1].replace(" ", "").replace("\"", "");
+			switch (name){
+			case "creator":
+				player = Bukkit.getPlayer(UUID.fromString(val));
+				break;
+			case "timeout_on":
+				timeoutOn = Double.parseDouble(val);
+				break;
+			case "timeout_off":
+				timeoutOff = Double.parseDouble(val);
+				break;
+			case "last_time":
+				lastTime = Double.parseDouble(val);
+				break;
+			}
+		}
+		return new SpamInput(player,timeoutOff,timeoutOn,lastTime);
 	}
 	
 	@Override
 	public String toString(){
-		return player.getUniqueId().toString() + ";" + timeoutOff + ";" + timeoutOn + ";" + lastTime;
+		return "{\"creator\": \"" + player.getUniqueId().toString() + "\", \"timeout_on\": " + timeoutOn + ", \"timeout_off\": " + timeoutOn + ", \"last_time\": " + lastTime + "}";
 	}
 	
 }
