eb24c944c565b8bb397ba31043f61050746e8a69
psrcek <p.srcek@gmail.com>
2016-11-10 22:09:49 +0100

    Fixed formatting

--- src/com/redstoner/javautils/check/Check.java
+++ src/com/redstoner/javautils/check/Check.java
@@ -28,43 +28,43 @@ import com.redstoner.moduleLoader.mysql.elements.MysqlDatabase;
 import com.redstoner.moduleLoader.mysql.elements.MysqlTable;
 
 public class Check extends Module implements Listener {
-
+	
 	MysqlTable table;
-
+	
 	@Override
 	public String getDescription() {
 		return "Get info on a player.";
 	}
-
+	
 	@Override
 	public String getName() {
 		return "Check";
 	}
-
+	
 	@Override
 	public void onEnable() {
-
+		
 		ModuleLoader loader = ModuleLoader.getLoader();
 		Map<String, String> config = loader.getConfiguration("Check.json");
-
+		
 		if (config == null || !config.containsKey("database") || !config.containsKey("table")) {
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Could not load the Check config file, disabling!");
-
+			
 			enabled = false;
 			return;
 		}
-
+		
 		try {
 			MysqlDatabase database = ModuleLoader.getLoader().getMysqlHandler().getDatabase(config.get("database"));
-
+			
 			table = database.getTable(config.get("table"));
 		} catch (NullPointerException e) {
 			Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Could not use the Check config, disabling!");
-
+			
 			enabled = false;
 			return;
 		}
-
+		
 	}
 	
 	// @formatter:off
@@ -79,45 +79,45 @@ public class Check extends Module implements Listener {
 				+ "}";
 	}
 	// @formatter:on
-
+	
 	@SuppressWarnings("deprecation")
 	@Command(hook = "checkCommand")
 	public void checkCommand(final CommandSender sender, final String player) {
 		msg(sender, "\n&2--=[ Check ]=--");
 		msg(sender, "&7Please notice that the data may not be fully accurate!");
 		OfflinePlayer oPlayer = Bukkit.getServer().getOfflinePlayer(player);
-
+		
 		Thread thread = new Thread(new Runnable() {
-
+			
 			@Override
 			public void run() {
 				sendData(sender, oPlayer);
 			}
-
+			
 		});
 		thread.setDaemon(true);
 		thread.start();
 	}
-
+	
 	public String read(URL url) {
 		String data = "";
 		try {
-
+			
 			Scanner in = new Scanner(new InputStreamReader(url.openStream()));
-
+			
 			while (in.hasNextLine()) {
 				data += in.nextLine();
 			}
-
+			
 			in.close();
-
+			
 			return data;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
+	
 	public JSONObject getIpInfo(OfflinePlayer player) {
 		String ip = "";
 		
@@ -134,9 +134,9 @@ public class Check extends Module implements Listener {
 		
 		try {
 			URL ipinfo = new URL("http://ipinfo.io/" + ip + "/json");
-
+			
 			String rawJson = read(ipinfo);
-
+			
 			return (JSONObject) new JSONParser().parse(rawJson);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -144,21 +144,21 @@ public class Check extends Module implements Listener {
 		
 		return null;
 	}
-
+	
 	public String getFirstJoin(OfflinePlayer player) {
 		Long firstJoin = player.getFirstPlayed();
 		Date date = new Date(firstJoin);
 		SimpleDateFormat format = new SimpleDateFormat("y-M-d H:m");
 		return format.format(date);
 	}
-
+	
 	public String getLastSeen(OfflinePlayer player) {
 		Long lastSeen = player.getLastPlayed();
 		Date date = new Date(lastSeen);
 		SimpleDateFormat format = new SimpleDateFormat("y-M-d H:m");
 		return format.format(date);
 	}
-
+	
 	public Object[] getWebsiteData(OfflinePlayer player) {
 		MysqlConstraint constraint = new MysqlConstraint("uuid", ConstraintOperator.EQUAL, player.getUniqueId().toString().replace("-", ""));
 		
@@ -172,11 +172,11 @@ public class Check extends Module implements Listener {
 			return new Object[] {null};
 		}
 	}
-
+	
 	public String getCountry(JSONObject data) {
 		return (String) data.get("country");
 	}
-
+	
 	public String getAllNames(OfflinePlayer player) {
 		String uuid = player.getUniqueId().toString().replace("-", "");
 		String nameString = "";
@@ -184,29 +184,29 @@ public class Check extends Module implements Listener {
 			String rawJson = read(new URL("https://api.mojang.com/user/profiles/" + uuid + "/names"));
 			System.out.println("name for " + uuid + " : " + rawJson);
 			JSONArray names = (JSONArray) new JSONParser().parse(rawJson);
-
+			
 			for (Object obj : names) {
 				nameString += ((JSONObject) obj).get("name") + ", ";
 			}
-
+			
 			nameString = nameString.substring(0, nameString.length() - 2);
-
+			
 			return nameString;
 		} catch (MalformedURLException | ParseException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
+	
 	public void msg(CommandSender sender, String message) {
 		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
 	}
-
+	
 	public void sendData(CommandSender sender, OfflinePlayer player) {
 		JSONObject ipInfo = getIpInfo(player);
-
+		
 		try {
-			//data
+			// data
 			String firstJoin = getFirstJoin(player);
 			String lastSeen = getFirstJoin(player);
 			firstJoin = (firstJoin.equals("1970-1-1 1:0")) ? "&eNever" : "&7(y-m-d h:m:s) &e" + firstJoin;
@@ -222,7 +222,7 @@ public class Check extends Module implements Listener {
 			String namesUsed = getAllNames(player);
 			if (namesUsed == null) namesUsed = "None";
 			
-			//messages
+			// messages
 			msg(sender, "&7Data provided by Redstoner:");
 			msg(sender, "&6>  UUID: &e" + player.getUniqueId());
 			msg(sender, "&6>  First joined: " + firstJoin);
