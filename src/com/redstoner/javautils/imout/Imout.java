f565e53eafc105383dbfb1932b544f26c0ead58f
MrRedSheep <daan1608@icloud.com>
2016-11-19 17:40:00 +0100

    removed TODOs and fixed name

--- src/com/redstoner/javautils/imout/Imout.java
+++ src/com/redstoner/javautils/imout/Imout.java
@@ -11,49 +11,50 @@ import com.nemez.cmdmgr.Command;
 import com.redstoner.javautils.adminchat.AdminChat;
 import com.redstoner.moduleLoader.Module;
 
-public class Imout extends Module{
-
+public class Imout extends Module {
+	
 	List<String> imout_toggle_list = new ArrayList<String>();
+	
 	@Override
 	public String getDescription() {
 		return "Adds /imout, displays fake leave/join messages";
 	}
-
+	
 	@Override
 	public String getName() {
-		return "Imout";
+		return "imout";
 	}
 	
 	@Command(hook = "imout")
-	public void on_imout_command(CommandSender sender){
-		if (!(sender instanceof Player)){
+	public void onImoutCommand(CommandSender sender) {
+		if (sender instanceof Player) {
 			sender.sendMessage("§cThis command can't be run from the console");
 		}
 		
-		else{
+		else {
 			Player s = (Player) sender;
 			String name = sender.getName();
-			if(imout_toggle_list.contains(name)){
-				 String symbol = "§a§l+";
-				 sender.sendMessage("§eWelcome back! You are no longer hidden");
-				 sender.sendMessage("§6We disabled /act for you!");
-				 s.performCommand("vanish off");
-				 imout_toggle_list.remove(name);
-				 Bukkit.broadcastMessage(symbol + " §7" + name);
-				 if(AdminChat.ac_toggle_list.contains(name)){
-					 AdminChat.ac_toggle_list.remove(name);
-				 }
+			if (imout_toggle_list.contains(name)) {
+				String symbol = "§a§l+";
+				sender.sendMessage("§eWelcome back! You are no longer hidden");
+				sender.sendMessage("§6We disabled /act for you!");
+				s.performCommand("vanish off");
+				if (AdminChat.ac_toggle_list.contains(name)) {
+					AdminChat.ac_toggle_list.remove(name);
+				}
+				imout_toggle_list.remove(name);
+				Bukkit.broadcastMessage(symbol + " §7" + name);
 			}
 			
-			else{
+			else {
 				String symbol = "§c§l-";
 				sender.sendMessage("§eYou just left... Or didn't you?");
 				imout_toggle_list.add(name);
-				s.sendMessage("§6We enabled /act for you!");
+				s.sendMessage("&6We enabled /act for you!");
 				s.performCommand("vanish on");
-				if(!(AdminChat.ac_toggle_list.contains(name))){
-					 AdminChat.ac_toggle_list.add(name);
-				 }
+				if (!(AdminChat.ac_toggle_list.contains(name))) {
+					AdminChat.ac_toggle_list.add(name);
+				}
 				Bukkit.broadcastMessage(symbol + " §7" + name);
 			}
 		}
